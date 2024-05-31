4ebdb348b391fab429cc3cc13c83aca56e599e6f
DESKTOP-RHMUDV9\Ethili Sundaravel <ethilispsdpe@gmail.com>
2024-05-31 15:27:24 -0400

    Email Invitation

--- Test2/Controllers/EmailController.cs
+++ Test2/Controllers/EmailController.cs
@@ -33,6 +33,74 @@ namespace Test2.Controllers
         {
             try
             {
+                // Get SMTP configuration and predefined email details from appsettings.json
+                string smtpServer = _configuration["SmtpSettings:Server"];
+                int smtpPort = int.Parse(_configuration["SmtpSettings:Port"]);
+                string smtpUsername = _configuration["SmtpSettings:Username"];
+                string smtpPassword = _configuration["SmtpSettings:Password"];
+                string subject = _configuration["SmtpSettings:Subject"];
+                string bodyTemplate = _configuration["SmtpSettings:Body"];
+
+                // Get all employees
+                var employees = _context.Employee.ToList();
+
+                using (SmtpClient smtpClient = new SmtpClient(smtpServer))
+                {
+                    smtpClient.UseDefaultCredentials = false;
+                    smtpClient.Port = smtpPort;
+                    smtpClient.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+                    smtpClient.EnableSsl = true;
+
+                    foreach (var employee in employees)
+                    {
+                        string recipientEmail = employee.Email;
+                        string acceptLink = Url.Action("AcceptInvitation", "Email", new { id = employee.Id }, Request.Scheme);
+                        string body = bodyTemplate.Replace("{AcceptLink}", acceptLink);
+
+                        using (MailMessage mailMessage = new MailMessage())
+                        {
+                            mailMessage.From = new MailAddress(smtpUsername);
+                            mailMessage.To.Add(recipientEmail);
+                            mailMessage.Subject = subject;
+                            mailMessage.Body = body;
+                            mailMessage.IsBodyHtml = true; // Set to true if using HTML in the body
+
+                            // Send the email
+                            smtpClient.Send(mailMessage);
+                        }
+                    }
+                }
+
+                ViewBag.Message = "Emails sent successfully!";
+            }
+            catch (SmtpException smtpEx)
+            {
+                // Log detailed SMTP exception
+                ViewBag.Message = "Error sending email: SMTP Exception - " + smtpEx.Message;
+                if (smtpEx.InnerException != null)
+                {
+                    ViewBag.Message += " Inner Exception - " + smtpEx.InnerException.Message;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log general exception
+                ViewBag.Message = "Error sending email: General Exception - " + ex.Message;
+                if (ex.InnerException != null)
+                {
+                    ViewBag.Message += " Inner Exception - " + ex.InnerException.Message;
+                }
+            }
+
+            return View("Index");
+        }
+
+
+        [HttpPost]
+        public IActionResult SendEmail1111()
+        {
+            try
+            {
                 Console.WriteLine($"SMTP Details");
                 // Get SMTP configuration and predefined email details from appsettings.json
                 string smtpServer = _configuration["SmtpSettings:Server"];
@@ -41,10 +109,13 @@ namespace Test2.Controllers
                 string smtpPassword = _configuration["SmtpSettings:Password"];
                 Console.WriteLine($"SMTP Username: {smtpUsername}");
                 Console.WriteLine($"SMTP Password: {smtpPassword}");
+                Console.WriteLine($"SMTP smtpServer: {smtpServer}");
+                Console.WriteLine($"SMTP smtpPort: {smtpPort}");
                 string recipientEmail = _configuration["SmtpSettings:RecipientEmail"];
                 string subject = _configuration["SmtpSettings:Subject"];
                 string body = _configuration["SmtpSettings:Body"];
-
+                Console.WriteLine($"SMTP recipientEmail: {recipientEmail}");
+                Console.WriteLine($"SMTP subject: {subject}");
                 // Configure the SMTP client
                 using (SmtpClient smtpClient = new SmtpClient(smtpServer))
                 {
@@ -62,6 +133,7 @@ namespace Test2.Controllers
                         mailMessage.Body = body;
                         mailMessage.IsBodyHtml = true; // Set to true if using HTML in the body
 
+                        Console.WriteLine($"SMTP sending mail body: {body}");
                         // Send the email
                         smtpClient.Send(mailMessage);
                     }
@@ -113,5 +185,22 @@ namespace Test2.Controllers
 
             return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Employees.xlsx");
         }
+
+        public IActionResult AcceptInvitation(int id)
+        {
+            var employee = _context.Employee.FirstOrDefault(e => e.Id == id);
+            if (employee != null)
+            {
+                employee.InvitationAccepted = true;
+                _context.SaveChanges();
+                ViewBag.Message = "Thank you for accepting the invitation!";
+            }
+            else
+            {
+                ViewBag.Message = "Invalid invitation.";
+            }
+            return View();
+        }
+
     }
 }
